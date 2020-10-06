5480a8223f6532cf4d9601d63de500a07707c6be
Cedric Don Coloma <colomce@oocl.com>
2020-10-06 09:56:42 +0800

    Felidae family class which inherits the animal class

---
+++ src/Felidae.java
@@ -0,0 +1,10 @@
+public class Felidae extends Animal {
+
+    public Felidae(int age, float weight) {
+        super(age, weight);
+    }
+
+    public void meow() {
+        System.out.println("meowing meowing");
+    }
+}
